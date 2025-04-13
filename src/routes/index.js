921d2875db007e0eb48c0d517054414e6f4bcdae
Pellumi <pellumii67@gmail.com>
2025-04-13 20:01:58 +0100

    feat: Set up routing for authentication and FAQ endpoints

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 import express from "express";
+import authRoutes from "./auth.js";
+import faqRoutes from "./faq.js";
 
 const router = express.Router();
 
+router.use("/auth", authRoutes);
+router.use("/faq", faqRoutes);
 
 export default router;
